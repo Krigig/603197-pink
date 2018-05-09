b568b085f188b0bd83275e7ff2b1b0631765c6b2
Kristina <vasileva_kri@list.ru>
2018-05-09 20:23:10 +0200

    gitignore

--- gulpfile.js
+++ gulpfile.js
@@ -43,7 +43,7 @@ gulp.task("images", function() {
 gulp.task("webp", function() {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 
 });
 
@@ -99,6 +99,7 @@ gulp.task("build", function(done) {
     "copy",
     "style",
     "images",
+    "webp",
     "sprite",
     done
   );
